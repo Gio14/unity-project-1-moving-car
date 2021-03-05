893335e024f1531bb20f98c19d9492287af586ca
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-03-05 21:46:10 +0100

    Project 1

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+   
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, -3, 11);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CameraPosition();
+    }
+
+
+    Vector3 CameraPosition()
+    {
+        transform.position = player.transform.position + offset;
+
+        return transform.position;
+
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour 
+{
+   
+    private Rigidbody playerRb;
+
+     [SerializeField]private float horsePower = 25.0f;
+
+    private float horizontalInput;
+
+    private float verticalInput;
+
+    public int vehicleWheels=4;
+
+    [SerializeField] float turnSpeed = 5.0f;
+
+    [SerializeField] float speed;
+
+    [SerializeField] float rpm;
+
+    [SerializeField] int wheelsOnGround;
+
+    [SerializeField] GameObject centerOfMass;
+
+    [SerializeField] TextMeshProUGUI speedometerText;
+
+    [SerializeField] TextMeshProUGUI rpmText;
+
+    [SerializeField] List<WheelCollider> allWheels;
+
+   
+   
+   
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+   
+    void FixedUpdate()
+    {
+
+        PlayerInput();
+
+        if (isOnGround())
+        {
+            CarMovement();
+
+            Speedometer();
+
+            Rpm();
+        }
+    }
+
+    bool isOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == vehicleWheels)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        
+    }
+
+    private void CarMovement()
+    {
+        //playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+
+        playerRb.AddRelativeForce(Vector3.forward * horsePower*verticalInput );
+        
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * turnSpeed);
+    }
+
+    private void Speedometer()
+    {
+        speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);//3.6 for kph
+        speedometerText.SetText("Speed: " + speed + " mph");
+    }
+
+    private void Rpm()
+    {
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText("Rpm: " + rpm);
+    }
+
+    private void PlayerInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+    
+}
